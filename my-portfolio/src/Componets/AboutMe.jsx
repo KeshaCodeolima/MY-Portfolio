25dff3ebed146faf3a88faad07884b8c915dd595
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-08-24 10:47:34 +0530

    create the iteams that include in the about me page

--- my-portfolio/src/Componets/AboutMe.jsx
+++ my-portfolio/src/Componets/AboutMe.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Image1 from '../Images/Photo.jpg';
+import './AboutMe.css';
 
 function AboutMe() {
   return (
     <>
-    
+    <div className="aboutme">
+        <div className="image">
+            <img src={Image1} alt="Myphoto" className='image1'/>
+        </div>
+        <div className="text">
+            <p>I'm <span>Kehan Kulasekara</span>,Full-Stack Developer</p>
+            <p>I specialize in building modern and responsive Web Applications.</p>
+        </div>
+        <div className="button">
+            <button>Contact with me</button>
+            <button>Download CV</button>
+        </div>
+    </div>
     </>
   )
 }
