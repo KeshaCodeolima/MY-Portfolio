c3e4f906b1a990e6cbc2debb4bf2fddb9a908e81
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-08-25 12:22:37 +0530

    delete the all things from the about pages

--- my-portfolio/src/Componets/AboutMe.jsx
+++ my-portfolio/src/Componets/AboutMe.jsx
@@ -5,22 +5,6 @@ import './AboutMe.css';
 function AboutMe() {
   return (
     <>
-    <div className="aboutme">
-        <div className="line">
-            <hr />
-        </div>
-        <div className="image">
-            <img src={Image1} alt="Myphoto" className='image1'/>
-        </div>
-        <div className="text">
-            <p className='textp1'>I'm <span>Kehan Kulasekara</span>, Full-Stack Developer.</p>
-            <p className='textp2'>I specialize in building modern and responsive Web Applications.</p>
-        </div>
-        <div className="button1">
-            <button className='btn1'>Contact with me</button>
-            <button className='btn1'>Download CV</button>
-        </div>
-    </div>
     </>
   )
 }
