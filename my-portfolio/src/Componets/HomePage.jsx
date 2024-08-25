e23404abffd92ad19b39273f8e28c567103435de
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-08-25 12:18:59 +0530

    change the about page iteams to home page and copy the about page things to home page

--- my-portfolio/src/Componets/HomePage.jsx
+++ my-portfolio/src/Componets/HomePage.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './HomePage.css';
+import Image1 from '../Images/Photo.jpg';
 
 function HomePage() {
   return (
     <>
-      
+       <div className="aboutme">
+        <div className="line">
+            <hr />
+        </div>
+        <div className="image">
+            <img src={Image1} alt="Myphoto" className='image1'/>
+        </div>
+        <div className="text">
+            <p className='textp1'>I'm <span>Kehan Kulasekara</span>, Full-Stack Developer.</p>
+            <p className='textp2'>I specialize in building modern and responsive Web Applications.</p>
+        </div>
+        <div className="button1">
+            <button className='btn1'>Contact with me</button>
+            <button className='btn1'>Download CV</button>
+        </div>
+    </div>
     </>
   )
 }
