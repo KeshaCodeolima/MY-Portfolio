9d4daf7169dd38a814e609990f38f75e858f5daf
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-08-26 17:20:32 +0530

    import the Home page to App.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -2,11 +2,13 @@ import AboutMe from "./Componets/AboutMe";
 import ContactPage from "./Componets/ContactPage";
 import Navbar from "./Componets/Navbar";
 import ServicePage from "./Componets/ServicePage";
+import HomePage from "./Componets/HomePage";
 
 function App() {
   return (
     <>
     <Navbar/>
+    <HomePage/>
     <AboutMe/>
     <ServicePage/>
     <ContactPage/>
