f9968f58064e0bba8b76af33a2a83f50fc7abdb6
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-10 11:07:24 +0530

    Add the frist git repositorie

--- my-portfolio/src/Componets/GitPage.jsx
+++ my-portfolio/src/Componets/GitPage.jsx
@@ -4,7 +4,14 @@ import './GitPage.css';
 function GitPage (){
     return(
         <>
-        
+        <div className="gitmain">
+            <h3>Git Repositoires.</h3>
+            <div className="gitbox">
+                <img src="" alt="" />
+                <p>Tourism Web</p>
+                <button>GIT HUB</button>
+            </div>
+        </div>
         </>
     )
 }
