556d51e36cd56f299a408ed5a958aa6359489f8a
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-10 11:23:56 +0530

    Add some CSS to boxs in git page and change the margin top of the contact page

--- my-portfolio/src/Componets/GitPage.jsx
+++ my-portfolio/src/Componets/GitPage.jsx
@@ -7,9 +7,26 @@ function GitPage() {
             <div className="maingit">
                 <div className="gitmain">
                     <h3>Git Repositoires.</h3>
+                </div>
+                <div className="gitbox1">
                     <div className="gitbox">
                         <img src="" alt="" />
                         <p>Tourism Web</p>
+                        <button>GIT HUB</button>
+                    </div>
+                    <div className="gitbox">
+                        <img src="" alt="" />
+                        <p>Tourism Web1</p>
+                        <button>GIT HUB</button>
+                    </div>
+                    <div className="gitbox">
+                        <img src="" alt="" />
+                        <p>Tourism Web2</p>
+                        <button>GIT HUB</button>
+                    </div>
+                    <div className="gitbox">
+                        <img src="" alt="" />
+                        <p>Tourism Web3</p>
                         <button>GIT HUB</button>
                     </div>
                 </div>
