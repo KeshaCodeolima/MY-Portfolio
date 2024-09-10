5f91f3dbf4f612cdc13625722bb18617a67dd806
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-10 16:52:09 +0530

    change margin top size in service page and Add AOS

--- my-portfolio/src/Componets/ServicePage.jsx
+++ my-portfolio/src/Componets/ServicePage.jsx
@@ -1,6 +1,12 @@
 import './ServicePage.css';
+import AoS from 'aos';
+import 'aos/dist/aos.css'
+import { useEffect } from 'react';
 
 function ServicePage() {
+    useEffect(()=>{
+        AoS.init({duration:3000})
+    },[])
     return (
         <>
             <div className="serviceboxmain" id='service'>
@@ -8,22 +14,22 @@ function ServicePage() {
                     <h1>My Services.</h1>
                 </div>
                 <div className="servicebox1">
-                    <div className="boxtext">
+                    <div className="boxtext" data-aos="zoom-in-up">
                         <h4>Full-Stack Developer</h4>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, aliquid?</p>
                         <span>Read More</span>
                     </div>
-                    <div className="boxtext">
+                    <div className="boxtext" data-aos="zoom-in-up">
                         <h4>Frontend Developer</h4>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, aliquid?</p>
                         <span>Read More</span>
                     </div>
-                    <div className="boxtext">
+                    <div className="boxtext" data-aos="zoom-in-up">
                         <h4>Backend Developer</h4>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, aliquid?</p>
                         <span>Read More</span>
                     </div>
-                    <div className="boxtext">
+                    <div className="boxtext" data-aos="zoom-in-up">
                         <h4>Web Design</h4>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, aliquid?</p>
                         <span>Read More</span>
